2480d1432dc6f9b3c9896cca4372bd5d22b02742
ILia-1212 <iliasentiakov@gmail.com>
2022-06-03 02:17:00 +0300

    reflection

--- src/com/urise/webapp/MainReflection.java
+++ src/com/urise/webapp/MainReflection.java
@@ -3,17 +3,23 @@ package com.urise.webapp;
 import com.urise.webapp.model.Resume;
 
 import java.lang.reflect.Field;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
 
 public class MainReflection {
 
-    public static void main(String[] args) throws IllegalAccessException {
+    public static void main(String[] args) throws IllegalAccessException, ClassNotFoundException, NoSuchMethodException, InvocationTargetException {
         Resume r = new Resume();
-        Field field = r.getClass().getDeclaredFields()[0];
+        Class<? extends Resume> classResume = r.getClass();
+        Field field = classResume.getDeclaredFields()[0];
         field.setAccessible(true);
-        System.out.println("getName = " + field.getName());
-        System.out.println("get = " + field.get(r));
+        System.out.println("get Field Name = " + field.getName());
+        System.out.println("get Field Value = " + field.get(r));
         field.set(r, "new_uuid");
-        // to do  invoke r.toString via refl
-        System.out.println("get = " + field.get(r));
+        System.out.println("get Field (new)Value = " + field.get(r));
+
+        Method method = Class.forName(classResume.getCanonicalName()).getMethod("toString");
+        String result = (String) method.invoke(r, null);
+        System.out.println(result);
     }
 }
--- test/com/urise/webapp/storage/AbstractArrayStorageTest.java
+++ test/com/urise/webapp/storage/AbstractArrayStorageTest.java
@@ -11,13 +11,15 @@ public abstract class AbstractArrayStorageTest {
     private final Storage storage;
 
     private static final String UUID_1 = "uuid1";
-    private static final String UUID_2 = "uuid2";
-    private static final String UUID_3 = "uuid3";
-    private static final String UUID_4 = "uuid4";
-
     private static final Resume RESUME_1 = new Resume(UUID_1);
+
+    private static final String UUID_2 = "uuid2";
     private static final Resume RESUME_2 = new Resume(UUID_2);
+
+    private static final String UUID_3 = "uuid3";
     private static final Resume RESUME_3 = new Resume(UUID_3);
+
+    private static final String UUID_4 = "uuid4";
     private static final Resume RESUME_4 = new Resume(UUID_4);
 
     public AbstractArrayStorageTest(Storage storage) {
@@ -56,6 +58,11 @@ public abstract class AbstractArrayStorageTest {
         assertGet(r);
     }
 
+    @Test(expected = NotExistStorageException.class)
+    public void updateNotExist() throws Exception {
+        storage.update(RESUME_4);
+    }
+
     @Test
     public void getAll() throws Exception {
         Resume[] r_all = storage.getAll();
@@ -67,8 +74,26 @@ public abstract class AbstractArrayStorageTest {
     @Test
     public void save() throws Exception {
         storage.save(RESUME_4);
-        assertGet(RESUME_4);
         assertSize(4);
+        assertGet(RESUME_4);
+    }
+
+    @Test(expected = ExistStorageException.class)
+    public void saveExist() throws Exception {
+        storage.save(RESUME_1);
+    }
+
+    @Test(expected = StorageException.class)
+    public void saveOverflow() throws Exception {
+        storage.clear();
+        try {
+            for (int i = 0; i < AbstractArrayStorage.STORAGE_LIMIT; i++) {
+                storage.save(new Resume());
+            }
+        } catch (StorageException e) {
+            Assert.fail("overflow storage while inserting index ");
+        }
+        storage.save(RESUME_4);
     }
 
     @Test(expected = NotExistStorageException.class)
@@ -76,6 +101,11 @@ public abstract class AbstractArrayStorageTest {
         storage.delete(UUID_3);
         assertSize(2);
         assertGet(RESUME_3);
+    }
+
+    @Test(expected = NotExistStorageException.class)
+    public void deleteNotExist() throws Exception {
+        storage.delete(UUID_4);
     }
 
     @Test
@@ -90,39 +120,11 @@ public abstract class AbstractArrayStorageTest {
         storage.get(UUID_4);
     }
 
-    @Test(expected = StorageException.class)
-    public void saveOverflow() {
-        storage.clear();
-        try {
-            for (int i = 0; i < AbstractArrayStorage.STORAGE_LIMIT; i++) {
-                storage.save(new Resume());
-            }
-        } catch (StorageException e) {
-            Assert.fail("overflow storage while inserting index ");
-        }
-        storage.save(RESUME_4);
-    }
-
-    @Test(expected = NotExistStorageException.class)
-    public void deleteNotExist() throws Exception {
-        storage.delete(UUID_4);
-    }
-
-    @Test(expected = NotExistStorageException.class)
-    public void updateNotExist() throws Exception {
-        storage.update(RESUME_4);
-    }
-
-    @Test(expected = ExistStorageException.class)
-    public void saveExist() throws Exception {
-        storage.save(RESUME_1);
+    protected void assertGet(Resume r) {
+        Assert.assertEquals(r, storage.get(r.getUuid()));
     }
 
     protected void assertSize(int expected) {
         Assert.assertEquals(expected, storage.size());
-    }
-
-    protected void assertGet(Resume r) {
-        Assert.assertEquals(r, storage.get(r.getUuid()));
     }
 }
